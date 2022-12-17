25530a8d09f7e3416128e1af2dd30597d3ba17d6
Leonardo De Paula Requena Dominici <leonardodominici@Leonardos-MacBook-Pro-350.local>
2022-12-17 01:24:22 -0300

    first commit

---
+++ src/braziliantaxservice/services/BrazilInterestServices.java
@@ -0,0 +1,17 @@
+package braziliantaxservice.services;
+
+public class BrazilInterestServices implements InterestService {
+
+	private double interestRate;
+
+	public BrazilInterestServices(double interestRate) {
+
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+}
---
+++ src/braziliantaxservice/services/UsaInterestServices.java
@@ -0,0 +1,17 @@
+package braziliantaxservice.services;
+
+public class UsaInterestServices implements InterestService {
+
+	private double interestRate;
+
+	public UsaInterestServices(double interestRate) {
+
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+}
